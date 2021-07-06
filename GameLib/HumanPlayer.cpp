fe194211c49da2f4292f825dd64908df8a96e898
SB0599 <sbaspach.01@gmail.com>
2021-07-06 21:12:02 +0200

    print Methodes for HumanPlayer Implemented, barrier is not working this way

--- GameLib/HumanPlayer.cpp
+++ GameLib/HumanPlayer.cpp
@@ -1,3 +1,5 @@
+#pragma once
+#include <iostream>
 #include "ConsoleUtils.h"
 #include "HumanPlayer.h"
 
@@ -8,28 +10,50 @@ HumanPlayer::HumanPlayer(const Coordinate &startCoords, const std::vector<Consol
 
 void HumanPlayer::playerTurn()
 {
-    /*
+    Direction direction;
     int input = 0;
     printPlayerMenu();
-    // einlesen
-    switch (input)
+    int gameTrain = ConsoleUtils::readInteger();
+    switch (gameTrain)
     {
     case 0:
         printMoveMenu();
-        int input = ConsoleUtils::readInteger();
-        Direction direction = input;// input to direction
+        direction = (Direction)ConsoleUtils::readInteger();
         move(direction);
-        break;
+        return;
     case 1:
         printBarrierMenu();
         int posStart_x = ConsoleUtils::readInteger();
         int posStart_y = ConsoleUtils::readInteger();
         int posEnd_x = ConsoleUtils::readInteger();
         int posEnd_y = ConsoleUtils::readInteger();
-        Position posStart(posStart_x, posStart_y);
-        Position posEnd(posEnd_x, posEnd_y);
-        Barrier barrier(posStart, posEnd);
-        break;
+        Coordinate posStart(posStart_x, posStart_y);
+        Coordinate posEnd(posEnd_x, posEnd_y);
+        //Barrier barrier(posStart, posEnd);
+        return;
     }
-*/
+
+}
+
+void HumanPlayer::printPlayerMenu()
+{
+    std::cout << "\nChose your came train:\n"
+              << "0) Move your player\n"
+              << "1) Add barrier\n";
+}
+
+void HumanPlayer::printMoveMenu()
+{
+    std::cout << "Move your player:\n"
+              << "0) Up\n"
+              << "1) Down\n"
+              << "2) Left\n"
+              << "3) Right\n";
+}
+
+void HumanPlayer::printBarrierMenu()
+{
+    std::cout << "Add barrier:\n"
+              << "Coordinate X:"
+              << "Coordinate Y:";
 }
--- GameLib/HumanPlayer.h
+++ GameLib/HumanPlayer.h
@@ -6,4 +6,9 @@ class HumanPlayer : public AbstractPlayer
 public:
     explicit HumanPlayer(const Coordinate &startCoords, const std::vector<ConsoleUtils::Colors> &player_color);
     void playerTurn() override;
+
+    private:
+    void printPlayerMenu();
+    void printMoveMenu();
+    void printBarrierMenu();
 };
