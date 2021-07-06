ff55d582bdec2619d2ffb0442fe5a2ac99910be8
SB0599 <sbaspach.01@gmail.com>
2021-07-06 19:43:08 +0200

    Position.cpp and .h cleand from toStringPlayer() is coded in AbstractPlayer

--- GameLib/Position.cpp
+++ GameLib/Position.cpp
@@ -29,15 +29,6 @@ std::string Position::toString() const
     return " ";
 }
 
-std::string Position::toStringPlayer() const
-{
-    // TODO you might want to adapt this method so that the field is not always empty
-    // if a player is at this position an X should be put there (if possible colored.)
-    //oder hier fkt zum rausfinden welcher player: nachteil hier müsste Game eingebunden werden
-    //                                             vorteil brauchen nicht noch mehr fkt oder müssen hier parameter mitgeben
-    return "X";
-}
-
 bool Position::operator==(const Position &other) const
 {
     return Coordinate::operator==(other) && m_vertex.getId() == other.m_vertex.getId();
