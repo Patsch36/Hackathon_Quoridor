3a264232be4baa757bc684b9503487ed35e5817b
Patsch36 <scheichpatrick@gmail.com>
2021-07-07 15:55:49 +0200

    Added Header for Class Gamerules

---
+++ GameLib/GameRules.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "GameField.h"
+#include <memory>
+
+enum class ruleBreakes = {
+    OUTOFFIELD,                 // Player wanted to move outside the field
+    BARRIER,                    // Player wanted to move over a barrier
+    PLAYER                      // Player wanted to move to another player
+};
+
+
+
+class GameRules : GameField
+{
+private:
+    static ruleBreakes checkGameBorders();
+    static ruleBreakes checkBarrier();
+    static ruleBreakes checkPlayer();
+
+    std::shared_ptr<GameField> _field;
+
+public:
+    static ruleBreakes CheckMovement();
+    GameRules(std::shared_ptr<GameField> field) : _field(field);
+};
